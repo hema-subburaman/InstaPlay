4a161b3645fc7ff4f6f87a5ed5ec2ef811d5d4ef
hema-subburaman <iamhema2k6@gmail.com>
2026-06-08 18:15:15 +0530

    upto second page

--- movieDB/src/components/LoginForm.jsx
+++ movieDB/src/components/LoginForm.jsx
@@ -1,12 +1,34 @@
-function LoginForm({ handleLogin }){
-    return(
-        <div className="login-card">
-            <h1>Sign in</h1>
-            <p>Sign in to your Self Service Portal</p>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <button onClick={handleLogin}>LOG IN</button>
-        </div>
-    );
+function LoginForm({
+  username,
+  password,
+  setUsername,
+  setPassword,
+  handleLogin,
+}) {
+  return (
+    <div className="login-card">
+      <h1>Sign in</h1>
+      <p>Sign in to your Self Service Portal</p>
+
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={handleLogin}>
+        LOG IN
+      </button>
+    </div>
+  );
 }
+
 export default LoginForm;
--- movieDB/src/pages/Login.jsx
+++ movieDB/src/pages/Login.jsx
@@ -2,21 +2,37 @@ import Header from "../components/Header";
 import LoginForm from "../components/LoginForm";
 import "../styles/Login.css";
 import { useNavigate } from "react-router-dom";
-function Login(){
-    const navigate = useNavigate();
-    const handleLogin = () => {
-        console.log("Login button clicked");
-        navigate("/home");
-    };
-    return(
-        <>
-        <Header />
-        <div className="login-container">
-            <LoginForm handleLogin = {handleLogin} />
-        </div>
-        
-        </>
+import { useState } from "react";
 
-    );
+function Login() {
+  const navigate = useNavigate();
+
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    if (username === "hema" && password === "1234") {
+      navigate("/home");
+    } else {
+      alert("Invalid Username or Password");
+    }
+  };
+
+  return (
+    <>
+      <Header />
+
+      <div className="login-container">
+        <LoginForm
+          username={username}
+          password={password}
+          setUsername={setUsername}
+          setPassword={setPassword}
+          handleLogin={handleLogin}
+        />
+      </div>
+    </>
+  );
 }
+
 export default Login;
