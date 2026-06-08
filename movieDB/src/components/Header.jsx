0a8191f1038bbb3940d46f718ec17da28daa0f0e
hema-subburaman <iamhema2k6@gmail.com>
2026-06-08 13:41:27 +0530

    Project installed

--- movieDB/src/components/Header.jsx
+++ movieDB/src/components/Header.jsx
@@ -1,7 +1,14 @@
+import playIcon from "../assets/playIcon.svg";
 function Header(){
     return(
         <header className="header">
-
+            <div className="logo">
+                <span className="highlight">I</span>
+                <span className="logo-text">nsta Pl</span>
+                <img src={playIcon} alt="play" className="play-icon"/>
+                <span className="logo-text">y</span>
+                
+            </div>
         </header>
     );
 }
