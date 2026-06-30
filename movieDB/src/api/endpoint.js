206af891a256d638c90b50d4638572b6e94409a0
hema-subburaman <iamhema2k6@gmail.com>
2026-06-30 10:16:48 +0530

    Completed

--- movieDB/src/api/endpoint.js
+++ movieDB/src/api/endpoint.js
@@ -1,12 +1,14 @@
-const BASE_URL = "https://jwt-authentication-kfpk.onrender.com";
+const BASE_URL = "https://api.themoviedb.org";
 
 const VERSION = "3";
+
+const RENDER_URL = "https://jwt-authentication-kfpk.onrender.com";
 
 export const TOKEN_API = `${BASE_URL}/${VERSION}/authentication/token/new?api_key=019085ae8fd360fcd800ae1d44592de2`;
 
 export const LOGIN_API = `${BASE_URL}/${VERSION}/authentication/token/validate_with_login?api_key=019085ae8fd360fcd800ae1d44592de2`;
 
-export const MOVIE_API = `${BASE_URL}/movies`;
+export const MOVIE_API = `${RENDER_URL}/movies`;
 
 export const MOVIEDETAILS_API = `${BASE_URL}/${VERSION}/movie`;
 
